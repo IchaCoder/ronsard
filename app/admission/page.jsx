49dbb4192c99ee3cb09b07e60fd1a3f47ae81a52
Emmanuel Yeboah <ichabordyeboah247@gmail.com>
2024-11-16 07:39:17 +0000

    added shadcn

--- app/admission/page.jsx
+++ app/admission/page.jsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import QRCode from "qrcode.react";
 import React from "react";
-import QRCode from "react-qr-code";
+// import QRCode from "react-qr-code";
 
 const AdmissionPage = () => {
   return (
     <div className="px-4 lg:w-[60%] mx-auto">
-      {/* <QRCode value="http://localhost:3000/admission" className="mt-8" /> */}
+      <QRCode value="http://localhost:3000/admission" className="mt-8" />
       <h1 className="flex gap-3 items-center mt-4">
         <Image className="w-12 h-12" src="/logo.png" alt="logo" width={150} height={150} />
         <span className="text-4xl font-bold uppercase">Ecole Ronsard Bilingual School</span>
